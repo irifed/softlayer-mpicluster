1afb65dd305943cc1cc73ede37d1dfbbe21eb23e
Dima Rekesh <bmwshop@yahoo.com>
2014-08-01 11:21:32 -0700

    Update prepclusternode.sh

--- prepclusternode.sh
+++ prepclusternode.sh
@@ -14,10 +14,18 @@ echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 adduser --disabled-password --gecos "" $USER
 
 # enable passwordless ssh between cluster nodes for user $USER
-mkdir /home/$USER/.ssh
-chmod 0700 /home/$USER/.ssh
-ssh-keygen -t rsa -b 2048 -f /home/$USER/.ssh/id_rsa -q -N ""
-cat /home/$USER/.ssh/id_rsa.pub >> /home/$USER/.ssh/authorized_keys
-chmod 0600 /home/$USER/.ssh/authorized_keys
-chown -R $USER:$USER /home/$USER/.ssh
+#Dima -- the code below does not work if you're logged in as root because your home dir is typically /root, 
+# not /home/root
+#mkdir /home/$USER/.ssh
+#chmod 0700 /home/$USER/.ssh
+#ssh-keygen -t rsa -b 2048 -f /home/$USER/.ssh/id_rsa -q -N ""
+#cat /home/$USER/.ssh/id_rsa.pub >> /home/$USER/.ssh/authorized_keys
+#chmod 0600 /home/$USER/.ssh/authorized_keys
+#chown -R $USER:$USER /home/$USER/.ssh
 
+mkdir $HOME/.ssh
+chmod 0700 $HOME/.ssh
+ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -q -N ""
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+chmod 0600 $HOME/.ssh/authorized_keys
+chown -R $USER:$USER $HOME/.ssh
